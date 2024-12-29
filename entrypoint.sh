282a03f55563bf069784c2fc597094e75659e683
1lkei <62320562+1lkei@users.noreply.github.com>
2024-12-29 13:12:23 +0800

    debug: test build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 stop_services() {
+    echo "Stopping services..."
     kill -TERM $pid1 $pid2 2>/dev/null
     wait $pid1 $pid2
     exit 0
@@ -8,11 +9,9 @@ stop_services() {
 
 trap stop_services SIGTERM SIGINT
 
-echo "start mcsmanager daemon"
 cd /opt/mcsmanager/daemon/ && node --max-old-space-size=8192 --enable-source-maps app.js &
 pid1=$!
 
-echo "start mcsmanager web"
 cd /opt/mcsmanager/web/ && node --max-old-space-size=8192 --enable-source-maps app.js &
 pid2=$!
 
